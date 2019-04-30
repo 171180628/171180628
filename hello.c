445506211a72ed6e5e567c07a3312c7dac214506
171180628 <935219697@qq.com>
2019-04-30 21:06:23 +0800

    c program hello.c

---
+++ hello.c
@@ -0,0 +1,25 @@
+#include "hello.h"
+#include<stdio.h>
+
+Circle::Circle()
+{
+    this->r=5.0;
+}
+
+Circle::Circle(double R)
+{
+    this->r=R;
+}
+
+double Circle:: Area()
+{
+    return 3.14*r*r;
+}
+int main()
+{
+    int i;
+    scanf("%d",&i);
+    printf("%d",Circle c(i);
+    
+    return 0;
+}
